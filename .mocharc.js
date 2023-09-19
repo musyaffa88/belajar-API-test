cd4056cb5a8d1bff3dd610e31af599d34d44f96c
musyaffa88 <farismusyaffa88@gmail.com>
2023-09-19 16:25:35 +0700

    belajar API Test dengan supertest dan report portal

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	reporter: '@reportportal/agent-js-mocha',
+	'reporter-option': [
+		'endpoint=https://demo.reportportal.io/api/v1',
+		'apiKey=belajar_WY7MRNT4Qeavi3i_WSUEJCd2cPeF3yUPW0Y1FoSLLYLqSb9lZVBrpP483S0nBuOv',
+		'launch=Test API Dummy JSON',
+		'project=musyaffa88_personal',
+		'attributes=ApiTest;UnitTest',
+	],
+}
---
+++ main.js
@@ -0,0 +1,46 @@
+const request = require('supertest')('https://dummyjson.com')
+const chai = require('chai')
+const chaiJsonSchema = require('chai-json-schema')
+
+chai.use(chaiJsonSchema)
+const expect = chai.expect
+
+it('test json schema array', async function () {
+	const todosSchema = {
+		type: 'object',
+		properties: {
+			todos: {
+				type: 'array',
+				items: {
+					type: 'object',
+					properties: {
+						id: { type: 'number' },
+						todo: { type: 'string' },
+						userId: { type: 'number' },
+						completed: { type: 'boolean' },
+					},
+					required: ['id']
+				}
+			}
+		}
+	}
+
+	const res = await request.get('/todos')
+	expect(res.body).have.jsonSchema(todosSchema)
+})
+
+it('test json schema object', async function () {
+	const todoSchema = {
+		type: 'object',
+		properties: {
+			id: { type: 'number' },
+			todo: { type: 'string' },
+			userId: { type: 'number' },
+			completed: { type: 'boolean' },
+		},
+		required: ['id', 'todo', 'userId']
+	}
+
+	const res = await request.get('/todos/1')
+	expect(res.body).have.jsonSchema(todoSchema)
+})
